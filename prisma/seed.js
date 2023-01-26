16b1ce260efa1109abf88159eaaa1aa981c10a74
James Byrne <james.c.byrne1994@gmail.com>
2023-01-26 21:58:08 +0000

    core criteria done

--- prisma/seed.js
+++ prisma/seed.js
@@ -8,11 +8,50 @@ async function seed() {
         }
     });
 
-    console.log('Customer created', createdCustomer);
-
     // Add your code here
 
+    const createdContact = await prisma.contact.create({
+        data:{
+            phone: '085288765',
+            email: 'Luna@meow.com',
+            customer : {
+                create: 
+                    {name: 'placeholder'} 
+            }
+            
+        }
+    })
 
+    const movie = await prisma.movie.create({
+        data: {
+            title: "Lord of the rings",
+            runtimeMins: 686
+        }
+    })
+
+    const screen = await prisma.screen.create ({
+        data: {
+            number: 1,
+        }
+    })
+
+    const screening = await prisma.screening.create ({
+        data: {
+            startsAt: new Date(),
+            movieId: 1,
+            screenId: 1,
+        }
+    })
+
+    const ticket = await prisma.ticket.create({
+        data: {
+            customerId:1,
+            screeningId: 1,
+
+        }
+    })
+
+    
 
 
     // Don't edit any of the code below this line
